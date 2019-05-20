2360693d291175f85558425734a74a629b0450f0
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-20 18:21:11 -0400

    initial commit

---
+++ app/src/main/java/io/kaeawc/motionpuzzle/App.kt
@@ -0,0 +1,11 @@
+package io.kaeawc.motionpuzzle
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
