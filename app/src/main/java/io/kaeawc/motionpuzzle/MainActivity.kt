c7b2452947b2a42cc4efee961421a4345ec320fa
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-20 22:29:31 -0400

    complete working puzzle

--- app/src/main/java/io/kaeawc/motionpuzzle/MainActivity.kt
+++ app/src/main/java/io/kaeawc/motionpuzzle/MainActivity.kt
@@ -2,6 +2,11 @@ package io.kaeawc.motionpuzzle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionScene
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
